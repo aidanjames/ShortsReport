4ce4ee987e040902f02b1a2b4e9a9d3bd9b2d84b
AidanJP <aidanpendlebury@gmail.com>
2020-06-09 20:26:57 +0100

    Initial Commit

---
+++ ShortsReport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShortsReport
+//
+//  Created by Aidan Pendlebury on 09/06/2020.
+//  Copyright © 2020 Aidan Pendlebury. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
